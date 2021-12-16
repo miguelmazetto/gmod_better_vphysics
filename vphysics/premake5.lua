1abbc0488c548f7587b0de7442dce5e207c0fd85
miguelmazetto <33629994+miguelmazetto@users.noreply.github.com>
2021-12-16 17:47:33 -0300

    Fix build and start debugging

--- vphysics/premake5.lua
+++ vphysics/premake5.lua
@@ -9,15 +9,18 @@ project("vphysics")
 	targetdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
 	debugdir("%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}")
 	objdir("!%{prj.location}/%{cfg.architecture}/%{cfg.buildcfg}/intermediate/%{prj.name}")
-	libdirs({
-		"../ivp/projects/" .. os.target() .. "/" .. _ACTION
-	})
+	
+	--libdirs({
+	--	"../ivp/projects/" .. os.target() .. "/" .. _ACTION
+	--})
 
 	filter("platforms:x86")
 		libdirs({"../sourcesdk-minimal/lib/public"})
 
 	filter("platforms:x86_64")
 		libdirs({"../sourcesdk-minimal/lib/public/x64"})
+
+	filter({})
 
 	sysincludedirs({
 		"../sourcesdk-minimal/public",
@@ -32,6 +35,7 @@ project("vphysics")
 		"../ivp/havana/havok/hk_physics",
 		"../ivp/havana"
 	})
+
 	files({
 		"convert.cpp",
 		"../sourcesdk-minimal/public/filesystem_helpers.cpp",
@@ -53,19 +57,21 @@ project("vphysics")
 		"physics_virtualmesh.cpp",
 		"trace.cpp",
 		"vcollide_parse.cpp",
-		"vphysics_saverestore.cpp",
+		"vphysics_saverestore.cpp"
 	})
 	vpaths({["Source files/*"] = "*.cpp"})
 	
-	filter("platforms:x86","system:windows")
+	filter({"platforms:x86","system:windows"})
 		defines({"COMPILER_MSVC32","WIN32"})
 
-	filter("platforms:x86_64","system:windows")
+	filter({"platforms:x86_64","system:windows"})
 		defines({"COMPILER_MSVC64","WIN32","WIN64"})
 
 	filter("system:linux")
 		defines({"_LIB", "LINUX"})
 		buildoptions { "-fpic", "-fno-semantic-interposition" }
+
+	filter({})
 
 	IncludeIVP_hk_base()
 	IncludeIVP_hk_math()
@@ -78,3 +84,9 @@ project("vphysics")
 	--IncludeSDKTier2()
 	IncludeSDKMathlib()
 	--IncludeSDKvstdlib()
+
+	filter("system:windows")
+		debuggertype("NativeOnly")
+
+	filter({"system:windows","platforms:x86"})
+		debugcommand('cp -f --reply=yes D:/UserFiles/repos/gmod_better_vphysics/projects/windows/vs2019/x86/Debug/vphysics.dll C:/Program Files (x86)/Steam/steamapps/common/GarrysMod/bin && C:/Program Files (x86)/Steam/steamapps/common/GarrysMod/bin/gmod.exe')
